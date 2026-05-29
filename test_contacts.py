f706a71bafac871b6f78b42d0c1e6662b412227e
Anurag <gramji2006@gmail.com>
2026-05-29 17:01:04 +0530

    Add files via upload
    Third Week Python Project Submission

---
+++ test_contacts.py
@@ -0,0 +1,10 @@
+contacts = {
+    "Anurag": {
+        "phone": "9876543210",
+        "email": "anurag@gmail.com",
+        "group": "Friends"
+    }
+}
+
+print("Testing Contact Data")
+print(contacts)
